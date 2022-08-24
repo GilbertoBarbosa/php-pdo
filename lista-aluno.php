26c7a280ad1a432b8542fa366fda36a7b91b93f8
Gilberto Barbosa <gisantos555@gmail.com>
2022-08-24 11:42:57 -0300

    Capítulo 7

--- lista-aluno.php
+++ lista-aluno.php
@@ -2,26 +2,15 @@
 
 use Alura\Pdo\Domain\Model\Student;
 use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
 
 require_once 'vendor/autoload.php';
 
 $pdo = ConnectionCreator::createConnection();
+$repository = new PdoStudentRepository($pdo);
+$studentList = $repository->allStudents();
 
-$statement = $pdo->query('SELECT * FROM students');
-//var_dump($statement->fetchColumn(1)); exit();
-
-$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
-$student = [];
-
-foreach ($studentDataList as $studentData) {
-    $studentList[] = new Student($studentData['id'],  
-                                 $studentData['name'],
-                                 new \DateTimeImmutable($studentData['birth_date'])
-    );
-}
 
 var_dump($studentList);
 
-/*$studentList2 = $statement->fetchAll(PDO::FETCH_CLASS, Student::class);
-var_dump($studentList2);*/
 
